a0b747fc005ef69f90c56a4bdd5524072e6fa2d6
Miragecore <MirageKimYs@Gmail.com>
2022-07-26 17:55:43 +0900

    a

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,11 @@
+
+<svelte:head>
+  <title>Welcome</title>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
+
 <p> Test </p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="/about">About my site</a>
